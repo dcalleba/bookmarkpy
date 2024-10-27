41190d1dcf8ce62eca5bf32c08b93b0d8001c865
dcalleba <daniel@callebaut.org>
2024-10-27 23:22:35 +0100

    avec gitignore python

---
+++ cree_gitignore.py
@@ -0,0 +1,26 @@
+import os
+
+def create_gitignore():
+    # Spécifier le chemin du répertoire courant
+    current_directory = os.getcwd()
+
+    # Lister tous les fichiers et dossiers dans le répertoire courant
+    all_files = os.listdir(current_directory)
+
+    # Demander à l'utilisateur quels fichiers ne pas ignorer
+    print("Entrez les noms des fichiers à ne PAS ignorer (séparés par des espaces):
+          ")
+    files_to_track = input().split()
+
+    # Créer un fichier .gitignore
+    with open('.gitignore', 'w') as gitignore_file:
+        for filename in all_files:
+            # Ignorer le fichier .gitignore lui-même et les fichiers que l'utilisateur veut suivre
+            if filename != '.gitignore' and filename not in files_to_track:
+                gitignore_file.write(f'{filename}\n')
+
+    print("Le fichier .gitignore a été créé avec tous les fichiers du répertoire courant, sauf ceux spécifiés.")
+    print("Fichiers non ignorés:", ', '.join(files_to_track))
+
+# Exécuter la fonction
+create_gitignore()
